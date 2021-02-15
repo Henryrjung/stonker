46f7b6d7619a7fa4c9fbd269fd6d45dba29af713
HenryJ <henryjung667@gmail.com>
2021-02-15 14:17:57 -0600

    added placeholder navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/nav/Navbar";
 
 function App() {
   return (
-
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer(){
+    return(
+        <>
+        <div className='footer'>
+            <p>Placeholder</p>
+        </div>
+        </>
+    )
+}
+
+export default Footer;
