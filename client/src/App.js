d3cb8883fc74976f6ee19674b2fd76a1335b2ba9
Kaylee Horner <kayleeahorner@gmail.com>
2021-02-16 19:44:19 -0600

    nav bar update

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import Navbar from "./components/nav/Navbar";
+import Navbar from "./components/Navbar/Navbar";
+import LoginForm from "./components/LoginForm/LoginForm";
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
+        <LoginForm/>
       </div>
     </Router>
   );
