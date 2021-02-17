fb4191b24603585a92cb5ffb270229af8193d838
Kaylee Horner <kayleeahorner@gmail.com>
2021-02-16 19:20:58 -0600

    adding pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 function Home(){
-    
+
+   
+
 }
 
 export default Home;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,8 @@
 import React from "react";
+import LoginForm from "../components/LoginForm/LoginForm"
 
 function Login(){
-    
+    <LoginForm/>
 }
 
 export default Login;
