da7ce484a3cc3b5f857ca6c5d26c528348b0dcbb
Kaylee Horner <kayleeahorner@gmail.com>
2021-02-16 19:22:19 -0600

    adding

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import Navbar from "../src/components/Navbar/Navbar"
+import LoginForm from "../src/components/LoginForm/LoginForm"
 function App() {
   return (
 
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+  <LoginForm/>
     </div>
   );
 }
