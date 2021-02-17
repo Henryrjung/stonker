f1d8beb208e5afce6fecb817434f402c4960e273
HenryJ <henryjung667@gmail.com>
2021-02-17 15:53:19 -0600

    routed the home and login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,7 +2,11 @@ import React from "react";
 import LoginForm from "../components/LoginForm/LoginForm"
 
 function Login(){
-    <LoginForm/>
+    return(
+        <div>
+            <LoginForm />
+        </div>
+    )
 }
 
 export default Login;
