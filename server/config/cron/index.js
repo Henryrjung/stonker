a27d3dabd49482dfed215bb601bf39f52a341a7d
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2021-02-17 18:49:36 -0600

    finishes the initial work on cron index.js

--- server/config/cron/index.js
+++ server/config/cron/index.js
@@ -25,86 +25,92 @@ const standardDev = function(array) {
 };
 
 const search10Symbols = async function(date = new Date()) {
-  const yesterday = new Date(date.valueOf - 24 * 60 * 60 * 1000);
-  const companies = await db.Company.findAll({
-    include: [db.Trend],
-    // Op.lte should be less than or equal to
-    where: { checkedAt: { [Op.lte]: yesterday } }
-  });
+  try {
+    const yesterday = new Date(date.valueOf - 24 * 60 * 60 * 1000);
+    const companies = await db.Company.findAll({
+      include: [db.Trend],
+      // Op.lte should be less than or equal to
+      where: { checkedAt: { [Op.lte]: yesterday } }
+    });
 
-  const symbolsToSearch = companies.reduce(
-    (symbols, comp, idx) =>
-      idx < 10 ? [...symbols, comp.symbol] : [...symbols],
-    []
-  );
-
-  if (symbolsToSearch[0]) {
-    res = await Promise.all(
-      symbolsToSearch.map((symb) => searchTrends(symb, date))
+    const symbolsToSearch = companies.reduce(
+      (symbols, comp, idx) =>
+        idx < 10 ? [...symbols, comp.symbol] : [...symbols],
+      []
     );
 
-    res.forEach((singleRes) => {
-      const trendResults = JSON.parse(singleRes).default.timelineData;
-
-      const stdDev = standardDev(trendResults.map((obj) => obj.value[0]));
-
-      const dayifier = 24 * 60 * 60 * 1000;
-      const day6 = yesterday.valueOf / dayifier;
-      const dateMap = trendResults.reduce(
-        (map, trend) => {
-          const day = day6 - parseInt(trend.time) / dayifier;
-          switch (day) {
-            case 0:
-              map.set('day6', [...map.get('day6'), trend]);
-              break;
-            case 1:
-              map.set('day5', [...map.get('day5'), trend]);
-              break;
-            case 2:
-              map.set('day4', [...map.get('day4'), trend]);
-              break;
-            case 3:
-              map.set('day3', [...map.get('day3'), trend]);
-              break;
-            case 4:
-              map.set('day2', [...map.get('day2'), trend]);
-              break;
-            case 5:
-              map.set('day1', [...map.get('day1'), trend]);
-              break;
-            default:
-              return;
-          }
-        },
-        new Map([
-          ['day1', []],
-          ['day2', []],
-          ['day3', []],
-          ['day4', []],
-          ['day5', []],
-          ['day6', []]
-        ])
+    if (symbolsToSearch[0]) {
+      res = await Promise.all(
+        symbolsToSearch.map((symb) => searchTrends(symb, date))
       );
-      let dbData = {};
-      dateMap.forEach((day, key) => {
-        let max = 0;
-        const avg =
-          day.reduce((acc, trend) => {
-            const val = trend.value[0];
-            if (max < val) {
-              max = val;
+
+      res.forEach((singleRes, resIdx) => {
+        const trendResults = JSON.parse(singleRes).default.timelineData;
+
+        const stdDev = standardDev(trendResults.map((obj) => obj.value[0]));
+
+        const dayifier = 24 * 60 * 60 * 1000;
+        const day6 = yesterday.valueOf / dayifier;
+        const dateMap = trendResults.reduce(
+          (map, trend) => {
+            const day = day6 - parseInt(trend.time) / dayifier;
+            switch (day) {
+              case 0:
+                map.set('day6', [...map.get('day6'), trend]);
+                break;
+              case 1:
+                map.set('day5', [...map.get('day5'), trend]);
+                break;
+              case 2:
+                map.set('day4', [...map.get('day4'), trend]);
+                break;
+              case 3:
+                map.set('day3', [...map.get('day3'), trend]);
+                break;
+              case 4:
+                map.set('day2', [...map.get('day2'), trend]);
+                break;
+              case 5:
+                map.set('day1', [...map.get('day1'), trend]);
+                break;
+              default:
+                return;
             }
-            return acc * val;
-          }, 1) / day.length;
+          },
+          new Map([
+            ['day1', []],
+            ['day2', []],
+            ['day3', []],
+            ['day4', []],
+            ['day5', []],
+            ['day6', []]
+          ])
+        );
+        let dbData = {};
+        dateMap.forEach((day, key) => {
+          let max = 0;
+          const avg =
+            day.reduce((acc, trend) => {
+              const val = trend.value[0];
+              if (max < val) {
+                max = val;
+              }
+              return acc * val;
+            }, 1) / day.length;
 
-        dbData[key + 'avg'] = avg;
-        dbData[key + 'max'] = max;
+          dbData[key + 'avg'] = avg;
+          dbData[key + 'max'] = max;
+        });
+
+        dbData.standardDeviation = stdDev;
+        dbData.CompanyId = companies[resIdx];
+
+         await db.Trend.create(dbData);
+        db.Company.update({checkedAt: date}, {where: {id: dbData.CompanyId}})
       });
-
-      dbData.standardDeviation = stdDev;
-
-      db.Trend.create
-    });
+    }
+  } catch (err) {
+    console.error(err);
   }
 };
 
