7ef4c007f86b5df565ba1510edb925cb75dd1c9f
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2021-02-18 20:27:58 -0600

    fixes code to earlier working state

--- server/config/cron/searchTrends.js
+++ server/config/cron/searchTrends.js
@@ -1,9 +1,9 @@
 const googleTrends = require('google-trends-api');
+
 // timezone compared to UTC
 const TIMEZONE = -18;
 
 const searchTrends = function(input, date) {
-  console.log('searchTrends');
   const weekAgo = new Date(date - 6 * 24 * 60 * 60 * 1000);
   return googleTrends.interestOverTime({
     keyword: [input],
