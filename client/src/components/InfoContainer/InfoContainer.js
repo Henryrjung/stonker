4d6c1a9d573422d634da5d96f005d6f10ecd73b6
HenryJ <henryjung667@gmail.com>
2021-02-18 20:53:58 -0600

    fixed widget

--- client/src/components/InfoContainer/InfoContainer.js
+++ client/src/components/InfoContainer/InfoContainer.js
@@ -2,53 +2,53 @@ import React, { Component } from "react";
 // import Container from "./Container";
 // import SearchForm from "./SearchForm/SearchForm";
 import HitCard from "../HitCard/HitCard";
-import Container from '@material-ui/core/Container';
+import Container from "@material-ui/core/Container";
 import Ticker from "../Ticker/Ticker";
 
+import API from "../../utils/API";
+
 class InfoContainer extends Component {
-    // Copied from Emp Activity for Sorting and Loading Hits
-    // state = {
-    //     employees: [],
-    //     filterRun: [],
-    //     nameSort: 0,
-    //     search: "",
-    //   };
-    
-    //   componentDidMount() {
-    //     this.loadEmployees();
-    //   }
+  //   state = {
+  //     hits: [],
+  //     // filterRun: [],
+  //     // nameSort: 0,
+  //     // search: "",
+  //   };
 
-    // handleInputChange = (event) => {
-    //     const { value } = event.target;
-    //     const filterRun = this.state.employees.filter((emp) => 
-    //       emp.email.includes(value));
-    //       this.setState({
-    //         search: value, filterRun,
-    //       });
-    //     };
-     
-  
-    
+  //   componentDidMount() {
+  //     API.getTopHits().then((res) => {
+  //       const hits = res.data.data.children.map((obj) => obj.data);
+  //       this.setState({ hits });
+  //     });
+  //   }
 
+  // handleInputChange = (event) => {
+  //     const { value } = event.target;
+  //     const filterRun = this.state.employees.filter((emp) =>
+  //       emp.email.includes(value));
+  //       this.setState({
+  //         search: value, filterRun,
+  //       });
+  //     };
 
-render() {
-    return(
-        <Container maxWidth="sm">
-            <div>
-                <Ticker/>
-            {/* <SearchForm
+  render() {
+    return (
+      <Container maxWidth="sm">
+        <div>
+          <Ticker />
+          {/* <SearchForm
                 searchInput={this.state.search}
                 handleInputChange={this.handleInputChange}
                 filterRes={this.sorting}
             /> */}
-            </div>
-            <div>
-                <h2>Trending Hits</h2>
-            <HitCard/>
-            </div>
-        </Container>
-    )
-}
+        </div>
+        <div>
+          <h2>Trending Hits</h2>
+          <HitCard />
+        </div>
+      </Container>
+    );
+  }
 }
 
 export default InfoContainer;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,22 @@
 import React from "react";
-import InfoContainer from '../components/InfoContainer/InfoContainer';
+import InfoContainer from "../components/InfoContainer/InfoContainer";
+import TradingViewWidget, { Themes } from "react-tradingview-widget";
+import Ticker from "../components/Ticker/Ticker";
 
-function Home(){
-    return(
-        <div>
-            <InfoContainer />
-        </div>
-    )
+function Home() {
+  return (
+    <div>
+      <Ticker />
+      <TradingViewWidget
+        symbol="NASDAQ:AAPL"
+        theme={Themes.LIGHT}
+        locale="us"
+        width="600"
+        height="300"
+      />
+      <InfoContainer />
+    </div>
+  );
 }
 
 export default Home;
