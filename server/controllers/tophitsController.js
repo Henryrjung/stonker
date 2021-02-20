26a6afb00b3529e7eed785c365f849711b0246af
mattyparty <matthewpewewardy@gmail.com>
2021-02-19 19:44:15 -0600

    model change

--- server/controllers/tophitsController.js
+++ server/controllers/tophitsController.js
@@ -3,7 +3,8 @@ const db = require('../models');
 // define models for top hits controller
 module.exports = {
   findAll: function(req, res) {
-    db.TopHit.findAll()
+    db.top_hits
+      .findAll()
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   }
