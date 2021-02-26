301bffd7bdc96a97aeaea5b930298676e8cbb5ac
HenryJ <henryjung667@gmail.com>
2021-02-25 20:09:22 -0600

    added join

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(passport.session());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 // Add routes, API
