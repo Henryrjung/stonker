ddc14732cac1ec4f5e364ab8464b3517b27e273c
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2021-02-28 10:54:24 -0600

    updates cron dates to all occur at midnight UTC and fixes previous functionality that attempted to do so

--- server/config/cron/searchTrends.js
+++ server/config/cron/searchTrends.js
@@ -1,19 +1,14 @@
 const googleTrends = require('google-trends-api');
 
-// timezone compared to UTC
-const TIMEZONE = -18;
-
 const searchTrends = function(input, date) {
   let today = new Date(date);
   let weekAgo = new Date(date - 6 * 24 * 60 * 60 * 1000);
-  today.setUTCHours(0, 0, 0, 0);
-  weekAgo.setUTCHours(0, 0, 0, 0);
   return googleTrends.interestOverTime({
     keyword: [input],
     startTime: weekAgo,
-    endTime: date,
-    timezone: TIMEZONE * 60,
-    granularTimeResolution: true
+    endTime: today,
+    granularTimeResolution: true,
+    category: 12
   });
 };
 
